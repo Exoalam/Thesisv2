782438df551bcb237a8d2e70909a85a4661ec1b5
Nafiul Alam <nafiul.arpon@gmail.com>
2024-02-24 20:22:20 +0600

    update

--- E_B.py
+++ E_B.py
@@ -4,7 +4,7 @@ from normalizer import normalize
 model = AutoModelForSeq2SeqLM.from_pretrained("csebuetnlp/banglat5_nmt_en_bn")
 tokenizer = AutoTokenizer.from_pretrained("csebuetnlp/banglat5_nmt_en_bn", use_fast=False)
 
-input_sentence = ""
+input_sentence = "How are you?"
 input_ids = tokenizer(normalize(input_sentence), return_tensors="pt").input_ids
 generated_tokens = model.generate(input_ids)
 decoded_tokens = tokenizer.batch_decode(generated_tokens)[0]
