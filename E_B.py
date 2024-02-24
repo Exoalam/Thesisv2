350a5b1858f60888f1f7adc5143dbe640a835ff8
Nafiul Alam <nafiul.arpon@gmail.com>
2024-02-24 19:26:32 +0600

    e_b update

---
+++ E_B.py
@@ -0,0 +1,12 @@
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from normalizer import normalize 
+
+model = AutoModelForSeq2SeqLM.from_pretrained("csebuetnlp/banglat5_nmt_en_bn")
+tokenizer = AutoTokenizer.from_pretrained("csebuetnlp/banglat5_nmt_en_bn", use_fast=False)
+
+input_sentence = ""
+input_ids = tokenizer(normalize(input_sentence), return_tensors="pt").input_ids
+generated_tokens = model.generate(input_ids)
+decoded_tokens = tokenizer.batch_decode(generated_tokens)[0]
+
+print(decoded_tokens)
